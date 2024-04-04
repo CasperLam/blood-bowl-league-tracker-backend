f5d3207e33a933a87b1928ce082c253904f05872
Casper Lam <casper.lam.kh@googlemail.com>
2024-04-04 12:24:49 +0100

    add routes

--- routes/leagueRoute.js
+++ routes/leagueRoute.js
@@ -2,4 +2,6 @@ const express = require(`express`);
 const router = express.Router();
 const leagueController = require(`../controllers/leagueController`);
 
-router.route(`/:user_id`);
+router.route(`/:user_id`).get(leagueController.getLeagues);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const app = express();
 require(`dotenv`).config();
 const port = process.env.PORT || 5050;
 const cors = require(`cors`);
+const leagueRoutes = require(`./routes/leagueRoute`);
 
 app.use(cors(process.env.CORS_ORIGIN));
 app.use(express.json());
+
+app.use(`/api/leagues`, leagueRoutes);
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
